56cd6fea2405823fb8be65dc51a0e7ff9452bc28
Jason Moiron <jmoiron@jmoiron.net>
2011-10-04 15:08:40 -0400

    packageize speedparser, remove old experimental parallelization tests

---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. speedparser documentation master file, created by sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+speedparser
+-----------
+
+.. automodule:: speedparser
+
+.. highlight:: sh
+
+You can install speedparser with pip::
+
+    pip install speedparser
+
+You can fork speedparser `from its hg repository
+<http://bitbucket.org/jmoiron/speedparser/>`_::
+
+    hg clone http://bitbucket.org/jmoiron/speedparser/
+
+.. highlight:: python
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Setup script for speedparser."""
+
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+# some trove classifiers:
+
+# License :: OSI Approved :: MIT License
+# Intended Audience :: Developers
+# Operating System :: POSIX
+
+setup(
+    name='speedparser',
+    version=version,
+    description="feedparser but faster and worse",
+    long_description=open('README.rst').read(),
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+    keywords='feedparser rss atom rdf lxml',
+    author='Jason Moiron',
+    author_email='jason@hiidef.com',
+
+    url='https://github.com/hiidef/speedparser/',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="tests",
+    install_requires=[
+      # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
